db698c7e94888b46d4c664e4a40a54ca286d1efb
Joker <george_02@abv.bg>
2024-07-02 16:34:49 +0300

    Basic Form

--- AppProject/src/App.jsx
+++ AppProject/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import LogInPage from './LogIn'
 
 function App() {
 
   return (
     <>
+    <LogInPage />
     </>
   )
 }
--- AppProject/src/LogIn.jsx
+++ AppProject/src/LogIn.jsx
@@ -4,10 +4,21 @@ function LogInPage(){
 
     return (
         <>
-        
+            <div>
+                <form>
+                    <label>Username:</label>
+                    <input type="text" /><br></br>
+                    <label>Password:</label>
+                    <input type="text"></input><br></br>
+                    <input type="submit"></input>
+
+                </form>
+            </div>
         </>
 
     )
 
 }
 
+
+export default LogInPage;
