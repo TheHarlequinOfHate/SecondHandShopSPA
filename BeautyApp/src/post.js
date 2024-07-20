b116d16e0eb8e940ac0c1b7925f5870d0920a7bf
Joker <george_02@abv.bg>
2024-07-20 14:16:05 +0300

    Againe

--- BeautyApp/src/post.js
+++
@@ -1,9 +0,0 @@
-fetch('http://localhost:3030/jsonstore/users', {
-    method: 'POST',
-    body: JSON.stringify({
-        username: `${usernameValue}`,
-    }),
-    headers: {
-        "Content-type": "application/json"
-    }
-}).then((response) => response.json()).then((json) => console.log(json))
